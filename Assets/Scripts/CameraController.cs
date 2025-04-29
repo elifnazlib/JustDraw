dafda604cfd1b7b6f8f82f2952077e614a8d1a6d
Elif Nazlı Böke <111501335+elifnazlib@users.noreply.github.com>
2025-04-29 15:04:08 +0300

    Camera LERP Movement When Game Ends

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,30 +4,64 @@ public class CameraController : MonoBehaviour
 {
     public float mouseSensitivity = 300f;
     public float rotationSpeed = 50f;
-    
+
     private float rotationAroundX = 0f;  // For vertical movement of the camera
     private float rotationAroundY = 0f;  // For horizontal movement of the camera
 
     [SerializeField] Transform playerBody;
 
+    public bool isGameEnded = false;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition = new Vector3(0, 80f, 0);
+
+    private Quaternion startRotation;
+    private Quaternion endRotation = Quaternion.Euler(90f, 0f, 0f);
+
+    private float desiredDuration = 5f;
+    private float elapsedTime;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {        
+    {
         Cursor.lockState = CursorLockMode.Locked; // Locks the cursor in the center of the view
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        // Camera follows the player's position and can look around
 
-        transform.position = new Vector3(playerBody.position.x, 9f, playerBody.position.z);
-        rotationAroundY += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        rotationAroundX += Input.GetAxis("Mouse Y") * (-1) * mouseSensitivity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            // Ending the game
 
-        rotationAroundX = Mathf.Clamp(rotationAroundX, -90f, 50f);
-        
-        transform.localEulerAngles = new Vector3(rotationAroundX, rotationAroundY, 0f);
+            isGameEnded = true;
+            startPosition = transform.position;
+            startRotation = transform.rotation;
+        }
+
+        if (isGameEnded == false)
+        {
+            // Camera follows the player's position and can look around
+
+            transform.position = new Vector3(playerBody.position.x, 9f, playerBody.position.z);
+            rotationAroundY += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            rotationAroundX += Input.GetAxis("Mouse Y") * (-1) * mouseSensitivity * Time.deltaTime;
+
+            rotationAroundX = Mathf.Clamp(rotationAroundX, -90f, 50f);
+
+            transform.localEulerAngles = new Vector3(rotationAroundX, rotationAroundY, 0f);
+        }
+
+        if (isGameEnded == true)
+        {
+            // When game ends, the camera lerps and looks down from the top
+
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / desiredDuration;
+
+            transform.SetPositionAndRotation(Vector3.Lerp(startPosition, endPosition, percentageComplete), Quaternion.Slerp(startRotation, endRotation, percentageComplete));
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,79 +3,94 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 20f;   // Speed of the player
-    public float rotationSpeed = 1000f; // How fast the player turns - smoothly
-    private float targetYaw; // Target angle for rotation around Y-axis
-    [SerializeField] Camera playerCamera;   // Camera following the player
-    public int colorChangeCount = 0;
+    public float rotationSpeed = 1000f;   // How fast the player turns - smoothly
+    private float targetYaw;   // Target angle for rotation around Y-axis
+    public int colorChangeCount = 0;   // For setting the first chosen color when game starts
+    private CameraController _cameraController;
 
     private void Start()
     {
-        targetYaw = transform.eulerAngles.y; // Saving the starting angle
+        targetYaw = transform.eulerAngles.y;   // Saving the starting angle
+        _cameraController = (CameraController)FindFirstObjectByType(typeof(CameraController));
     }
 
     void Update()
     {
-        Vector3 move = Vector3.zero;
-
-        // Inputs for going forward and backward
-
-        if (Input.GetKey(KeyCode.W))
+        if (_cameraController.isGameEnded == false)
         {
-            if (transform.position.x > 44f)
+            Vector3 move = Vector3.zero;
+
+            // Inputs for going forward and backward
+            // Player cannot exceed the boundaries
+
+            if (Input.GetKey(KeyCode.W))
             {
-                transform.position = new Vector3(43.8f, transform.position.y, transform.position.z);
+                if (transform.position.x > 44f)
+                {
+                    transform.position = new Vector3(43.8f, transform.position.y, transform.position.z);
+                }
+                else if (transform.position.x < -44f)
+                {
+                    transform.position = new Vector3(-43.8f, transform.position.y, transform.position.z);
+                }
+                else if (transform.position.z > 44f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, 43.8f);
+                }
+                else if (transform.position.z < -44f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, -43.8f);
+                }
+                else move += transform.forward;
             }
-            else if (transform.position.x < -44f)
+
+
+            if (Input.GetKey(KeyCode.S))
             {
-                transform.position = new Vector3(-43.8f, transform.position.y, transform.position.z);
+                if (transform.position.x > 44f)
+                {
+                    transform.position = new Vector3(43.8f, transform.position.y, transform.position.z);
+                }
+                else if (transform.position.x < -44f)
+                {
+                    transform.position = new Vector3(-43.8f, transform.position.y, transform.position.z);
+                }
+                else if (transform.position.z > 44f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, 43.8f);
+                }
+                else if (transform.position.z < -44f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, -43.8f);
+                }
+                else move -= transform.forward;
             }
-            else if (transform.position.z > 44f)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, 43.8f);
-            }
-            else if (transform.position.z < -44f)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, -43.8f);
-            }
-            else move += transform.forward;
+
+
+            transform.position += moveSpeed * Time.deltaTime * move;
+
+            // Inputs for turning right or left
+
+            if (Input.GetKeyDown(KeyCode.A))
+                targetYaw -= 90f;
+
+            if (Input.GetKeyDown(KeyCode.D))
+                targetYaw += 90f;
+
+
+            // Rotate the current angle to the target angle smoothly
+
+            float currentYaw = transform.eulerAngles.y;
+            currentYaw = Mathf.MoveTowardsAngle(currentYaw, targetYaw, rotationSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0f, currentYaw, 0f);
         }
-
-
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            if (transform.position.x > 44f)
-            {
-                transform.position = new Vector3(43.8f, transform.position.y, transform.position.z);
-            }
-            else if (transform.position.x < -44f)
-            {
-                transform.position = new Vector3(-43.8f, transform.position.y, transform.position.z);
-            }
-            else if (transform.position.z > 44f)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, 43.8f);
-            }
-            else if (transform.position.z < -44f)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, -43.8f);
-            }
-            move -= transform.forward;
+            Transform nose = transform.Find("nose");
+            // transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            // transform.GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+            nose.GetComponent<MeshRenderer>().enabled = false;
         }
-
-
-        transform.position += moveSpeed * Time.deltaTime * move;
-
-        // Inputs for turning right or left
-
-        if (Input.GetKeyDown(KeyCode.A))
-            targetYaw -= 90f;
-
-        if (Input.GetKeyDown(KeyCode.D))
-            targetYaw += 90f;
-
-        // Rotate the current angle to the target angle smoothly
-        float currentYaw = transform.eulerAngles.y;
-        currentYaw = Mathf.MoveTowardsAngle(currentYaw, targetYaw, rotationSpeed * Time.deltaTime);
-        transform.eulerAngles = new Vector3(0f, currentYaw, 0f);
     }
 }
